3f4d88972943b773426a3933a8b57383a19475c5
David <davidnlagrange@hotmail.com>
2019-08-08 12:26:10 -0500

    Intial Visual Studio Project Setup

---
+++ Query_Constructor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Query_Constructor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
